38a60abcd99e680e3df7c252e6727d72f022a563
vikash-verma-profile <er.vikashverma@yahoo.com>
2025-01-21 09:52:19 +0530

    added multi level

---
+++ labs/Day-2/multilevel.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+class A
+{
+public:
+    int a;
+    void getData()
+    {
+        cout << "enter value of a";
+        cin >> a;
+    }
+};
+
+class B : public A
+{
+public:
+    int b;
+    void getDataB()
+    {
+        cout << "enter value of b";
+        cin >> b;
+    }
+};
+class C : public B
+{
+private:
+    int c;
+
+public:
+    void getDataC()
+    {
+        cout << "enter value of c";
+        cin >> c;
+    }
+
+    void sum(){
+        int sum=a+b+c;
+        cout<<"Sum is :"<<sum;
+    }
+};
+
+int main()
+{
+
+    C obj;
+    obj.getData();
+    obj.getDataB();
+    obj.getDataC();
+    obj.sum();
+
+    return 0;
+}
